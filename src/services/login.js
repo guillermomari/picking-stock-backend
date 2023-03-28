32f0681fcd80afd142b0416cf19f2c0818f26bf2
guillermomari <guillermo.a.mari@gmail.com>
2023-03-28 19:37:01 -0300

    corregidos los modelo

--- src/services/login.js
+++ src/services/login.js
@@ -1,21 +1,26 @@
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
-const {User} = require('../orm/models');
+const { Users } = require('../orm/models');
 
 
-async function loginAuthentication (req,res) {
+async function loginAuthentication(req, res) {
   const { username, password } = req.body;
-  const user = await User.findOne({ username });
-  if (!user || !bcrypt.compareSync(password, user.password)) {
+  const user = await Users.findOne({
+    where: {
+      username: username,
+      password: password
+    }
+  });
+  if (!user ) {
     return res.status(401).json({ message: 'Usuario o contraseña incorrectos' });
   }
   let token = user.token;
+
   if (!token) {
-    token = jwt.sign({ userId: user._id, roleId: user.roleId }, process.env.JWT_SECRET, { expiresIn: process.env.TOKEN_EXPIRATION_TIME });
+    token = jwt.sign({ userId: user.id, roleId: user.roleId }, process.env.JWT_SECRET, { expiresIn: process.env.TOKEN_EXPIRATION_TIME });
     user.token = token;
     await user.save();
   }
   res.send({ token });
 }
 
-module.exports = {loginAuthentication}
+module.exports = { loginAuthentication }
--- src/utils/authenticationMiddleware.js
+++ src/utils/authenticationMiddleware.js
@@ -1,47 +1,45 @@
 const jwt = require('jsonwebtoken');
-const db = require('../orm/models'); // módulo para acceder a la base de datos
+const { Users } = require('../orm/models'); // módulo para acceder a la base de datos
+
 
 const secret = process.env.JWT_SECRET;
 
-function authMiddleware(req, res, next) {
+async function authMiddleware(req, res, next) {
   // obtener token del encabezado de autorización
   const authHeader = req.headers.authorization;
   if (!authHeader) {
-    return res.status(401).json({ message: 'No se proporcionó un token de autenticación' });
+    return res.send(401).json({ message: 'No se proporcionó un token de autenticación' });
   }
 
   // verificar que el token tenga el formato correcto
   const token = authHeader.split(' ')[1];
   if (!token) {
-    return res.status(401).json({ message: 'El formato proporcionado no corresponde a un token de autenticación' });
+    return res.send(401).json({ message: 'El formato proporcionado no corresponde a un token de autenticación' });
   }
 
-  // verificar y decodificar el token
+  const decoded = jwt.verify(token, secret); 
+  const user = await Users.findByPk(decoded.userId);
   try {
-    const decoded = jwt.verify(token, secret);
-    req.user = decoded;
+    
+    if (!user) {
+        res.send({ message: 'Usuario no encontrado en la base de datos' });
+    }
+    if (user.token !== token) {
+      console.log(user.token, token )
+        res.send({ message: 'Token de autenticación no válido' });
+    }
+    else {
+      res.send({ message: 'Acceso correctamente autenticado' })
+      next()
+    }
+
   } catch (err) {
-    return res.status(401).json({ message: 'Token de autenticación no válido' });
+     res.send({ message: 'Token de autenticación no válido' });
+
   }
 
-  // verificar si el usuario existe en la base de datos
-  const user = db.getUserById(req.user.userId);
-  if (!user) {
-    return res.status(401).json({ message: 'Usuario no encontrado en la base de datos' });
-  }
+  //VER TODO ESTO
 
-  // verificar si el token almacenado en la base de datos coincide con el token proporcionado
-  if (user.token !== token) {
-    return res.status(401).json({ message: 'Token de autenticación no válido' });
-  }
-
-  // verificar si el usuario tiene permiso para acceder al endpoint
-  if (!user.roles.includes(req.method + ' ' + req.path)) {
-    return res.status(403).json({ message: 'No tiene permiso para acceder a este endpoint' });
-  }
-
-  // pasar al siguiente middleware
-  next();
 }
 
 module.exports = authMiddleware;
